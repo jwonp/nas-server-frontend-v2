a96e0165110e6bfd5a9fbcf62e17237e7ca19a90
Joowon <tkdel222@gmail.com>
2024-01-06 22:05:35 +0900

    update history routing

--- src/components/Storage/DirectoryHistory.tsx
+++ src/components/Storage/DirectoryHistory.tsx
@@ -1,34 +1,46 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
 
 const DirectoryHistory = () => {
-  
-  const [historyList, setHistoryList] = useState<string[]>([
-    "root",
-    "folder",
-    "folder2",
-  ]);
+  const router = useRouter();
+  const historyList = useMemo(() => {
+    let history = router.query.history as string[] | null;
+
+    return history;
+  }, [router]);
+
   const historyBlocks = useMemo(() => {
-    const handleHistoryClick = (e: React.MouseEvent<HTMLDivElement>) => {
-      const index = parseInt(e.currentTarget.id);
-      setHistoryList(() => historyList.slice(0, index));
-    };
+    if (!historyList) {
+      return <div>/</div>;
+    }
+
     return historyList.map((history, index) => (
-      <div
+      <Link
         id={`${index}`}
         key={index}
-        onClick={handleHistoryClick}>{`/ ${history}`}</div>
+        href={`/storage/${historyList
+          .slice(0, index + 1)
+          .join("/")}`}>{`/ ${history}`}</Link>
     ));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [historyList]);
 
   const { isLoading, error, data } = useQuery({
     queryKey: ["history"],
-    queryFn: () => axios.get(`/api/storage/${historyList.join("/")}`).then((response) => response.data),
+    queryFn: () =>
+      axios
+        .get(`/api/storage/${historyList ? historyList.join("/") : ""}`)
+        .then((response) => response.data),
   });
+
   useEffect(() => {
     console.log(data);
   }, [data]);
+
   return (
     <div className="p-2 text-white text-2xl font-semibold font-['Inter']">
       <div className="flex gap-2">{historyBlocks}</div>
