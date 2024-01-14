e051c042363f4a7c6ed09134cc82e7fddcaf1c94
Joowon <tkdel222@gmail.com>
2024-01-14 22:05:52 +0900

    add /api/user/icon to upload user icon

---
+++ src/pages/api/user/icon.ts
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import S3 from "aws-sdk/clients/s3";
+import { randomUUID } from "crypto";
+
+const s3 = new S3({
+  apiVersion: "latest",
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.BUCKET_REGION,
+  signatureVersion: "v4",
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const fileType = req.query.fileType;
+  const key = `icons/${randomUUID()}.${fileType}`;
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: key,
+    Expires: 60,
+  };
+  const uploadUrl = s3.getSignedUrl(`putObject`, params);
+
+  res.status(200).json({
+    uploadUrl,
+    key: key,
+  });
+}
