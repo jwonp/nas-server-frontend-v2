37ae5808bca89e0a6a9eb4ae764711f5acaca11b
Joowon <tkdel222@gmail.com>
2024-01-15 22:26:43 +0900

    add /api/storage/meta

---
+++ src/pages/api/storage/meta.ts
@@ -0,0 +1,21 @@
+import { request } from "@/utils/request";
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { authOptions } from "../auth/[...nextauth]";
+import { getServerSession } from "next-auth/next";
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { metas } = req.body;
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    res.status(403).json({});
+  }
+
+  const result = await request(session?.user).post(
+    `${process.env.BACKEND_ENDPOINT}/storage/meta`,
+    metas
+  );
+
+  res.status(200).json(result.data);
+};
+
+export default handler;
