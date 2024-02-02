f1ca77374d5b5137fb244dbe19ad1cfa1f8fbd17
Joowon <tkdel222@gmail.com>
2024-02-02 19:01:44 +0900

    update delete file api

--- src/pages/api/storage/item/[[...path]].ts
+++ src/pages/api/storage/item/[[...path]].ts
@@ -3,17 +3,26 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { path } = req.query;
   const session = await getServerSession(req, res, authOptions);
-  if (!session || !session.user) {
-    return res.status(403).json({});
+  const { path } = req.query;
+  if (req.method === "GET") {
+    if (!session || !session.user) {
+      return res.status(403).json({});
+    }
+
+    const result = await request(session?.user).get(
+      `/storage/item?path=${path ? `/${(path as string[]).join("/")}` : ""}`
+    );
+
+    return res.status(200).json(result.data);
   }
-
-  const result = await request(session?.user).get(
-    `/storage/item?path=${path ? `/${(path as string[]).join("/")}` : ""}`
-  );
-
-  res.status(200).json(result.data);
+  if (req.method === "DELETE") {
+    const { fileId } = req.body;
+    const result = await request(session?.user).delete(`/storage/item`, {
+      data: { fileId: fileId, userId:session?.user.id },
+    });
+    return res.status(200).json(result.data);
+  }
 };
 
 export default handler;
