403757dcb88c0b649594a667e67e421889b98f82
Joowon <tkdel222@gmail.com>
2024-02-03 23:16:26 +0900

    add download func

---
+++ src/utils/download.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const downloadFile = async (key: string, fileName: string) => {
+  const url = await axios
+    .get(`/api/download?key=${key}`)
+    .then((res) => res.data.url);
+  const response = await fetch(url);
+  const file = await response.blob();
+  const downloadUrl = window.URL.createObjectURL(file);
+
+  const anchorElement = document.createElement("a");
+  document.body.appendChild(anchorElement);
+  anchorElement.download = fileName;
+  anchorElement.href = downloadUrl;
+
+  anchorElement.click();
+
+  document.body.removeChild(anchorElement);
+  window.URL.revokeObjectURL(downloadUrl);
+};
