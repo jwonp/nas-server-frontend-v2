43fab6750ffe2743c2b0628b00bf8a7076e4ca5f
Joowon <tkdel222@gmail.com>
2024-02-05 15:58:53 +0900

    fix typo

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,11 +1,12 @@
 import NextAuth, { DefaultSession } from "next-auth";
-import UserSession from "@/types/UserSession"
+import UserSession from "@/types/UserSession";
+
 declare module "next-auth" {
   interface Session {
     user: UserSession;
   }
   interface User {
-    id:string;
+    id: string;
     username: string;
     name: string;
     icon: string;
