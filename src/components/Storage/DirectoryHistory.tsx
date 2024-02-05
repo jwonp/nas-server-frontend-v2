d17d6b8fb44767fdafe75fed7d20cdce7e1ea40f
Joowon <tkdel222@gmail.com>
2024-02-05 15:48:50 +0900

    update to convert history key to name

--- src/components/Storage/DirectoryHistory.tsx
+++ src/components/Storage/DirectoryHistory.tsx
@@ -1,22 +1,56 @@
+import { DisplayHistoryResponse } from "@/types/Responses";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 
 const DirectoryHistory = () => {
   const router = useRouter();
-
-
+  const queryClient = useQueryClient();
+  const historyQuery = useQuery({
+    queryKey: ["history", { query: (router.query.history as string[]) ?? [] }],
+    queryFn: (): Promise<DisplayHistoryResponse[]> =>
+      axios
+        .get(
+          `/api/storage/directory/history?path=${
+            "/" + (router.query.history as string[])?.join("/")
+          }`
+        )
+        .then((res) => res.data),
+    enabled: (router.query.history as string[]) ? true : false,
+  });
+  useEffect(() => {
+    queryClient.invalidateQueries({
+      queryKey: [
+        "history",
+        { query: (router.query.history as string[]) ?? [] },
+      ],
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.query]);
   const historyBlocks = useMemo(() => {
-    const historyList =  router.query.history as string[] | null;
-    if (!historyList) {
+    if (!router.query.history || router.query.history.length === 0) {
       return (
         <Link
           id={"0"}
           href={`/storage`}>{`/ 내 드라이브`}</Link>
       );
     }
+    if (historyQuery.isLoading) {
+      return <div>/</div>;
+    }
+    const historyList = router.query.history as string[];
+    const displayHistoryMap = new Map<string, string>();
+    historyQuery.data?.forEach((history) => {
+      displayHistoryMap.set(history.key.split("folder$")[1], history.title);
+    });
+    const displayHistories = (router.query.history as string[]).map((hisotry) =>
+      displayHistoryMap.get(hisotry)
+    );
 
-    return ["내 드라이브", ...historyList].map((history, index) => {
+
+    return ["내 드라이브", ...displayHistories].map((history, index) => {
       if (index === 0) {
         return (
           <Link
@@ -36,7 +70,7 @@ const DirectoryHistory = () => {
     });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router]);
+  }, [historyQuery.data]);
 
   return (
     <div className="p-2 text-white text-2xl font-semibold font-['Inter']">
