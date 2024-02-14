58d695a334002a22f504259f820f7f45997fe6f6
Joowon <tkdel222@gmail.com>
2024-02-14 17:43:38 +0900

    update login message

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -20,6 +20,9 @@ const Layout = ({ children }: { children: JSX.Element }) => {
                 <div className=" text-center mb-4">
                   로그인이 필요한 서비스 입니다.
                 </div>
+                <div className=" text-center mb-4">
+                  아래 링크를 통해서 로그인 해주세요.
+                </div>
                 <div className="flex">
                   <div className="mx-auto text-center cursor-pointer underline decoration-solid" onClick={()=>{signIn()}}>
                     로그인
