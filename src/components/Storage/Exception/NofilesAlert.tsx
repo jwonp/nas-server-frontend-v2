fdd35c2a44bd3fe831154723348f98104249f286
Joowon <tkdel222@gmail.com>
2024-02-20 14:46:33 +0900

    update responsible css

--- src/components/Storage/Exception/NofilesAlert.tsx
+++ src/components/Storage/Exception/NofilesAlert.tsx
@@ -5,27 +5,34 @@ const NofilesAlert = () => {
   return (
     <article className="flex py-10">
       <div className="mx-auto">
-        <div className="flex">
-          <p className="leading-7 text-xl">우측 상단의</p>
-          <figure className="mx-1">
-            <Image
-              src={AddFolderIcon}
-              alt={""}
-              width={24}
-              height={24}
-            />
-          </figure>
-          <p className="leading-7 text-xl">나</p>
-          <figure className="mx-1">
-            <Image
-              src={AddFileIcon}
-              alt={""}
-              width={24}
-              height={24}
-            />
-          </figure>
-          <p className="leading-7 text-xl">
-            를 클릭해서 폴더 및 파일를 추가해주세요.
+        <div className="flex max-login:block">
+          <div className="flex">
+            <div className="max-login:mx-auto">
+              <div className="flex">
+                <p className="leading-7 text-xl text-center">우측 상단의</p>
+                <figure className="mx-1">
+                  <Image
+                    src={AddFolderIcon}
+                    alt={""}
+                    width={24}
+                    height={24}
+                  />
+                </figure>
+                <p className="leading-7 text-xl">나</p>
+                <figure className="mx-1">
+                  <Image
+                    src={AddFileIcon}
+                    alt={""}
+                    width={24}
+                    height={24}
+                  />
+                </figure>
+                <p className="leading-7 text-xl">를</p>
+              </div>
+            </div>
+          </div>
+          <p className="leading-7 text-xl text-center">
+            클릭해서 폴더 및 파일를 추가해주세요.
           </p>
         </div>
       </div>
