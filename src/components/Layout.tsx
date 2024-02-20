145b00d060b5943c76a780b1bbf5726d99a874de
Joowon <tkdel222@gmail.com>
2024-02-20 15:24:07 +0900

    update overlfow y hidden on main

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,41 +2,18 @@ import { useAppSelector } from "@/redux/hooks";
 import Header from "./Header/Header";
 import SideBar from "./SideBar/SideBar";
 import { getVisibleSideBar } from "@/redux/featrues/sideBarVisibleSlice";
-import { signIn, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import SigninAlert from "./SigninAlert";
 
 const Layout = ({ children }: { children: JSX.Element }) => {
   const isVisibleSidebar = useAppSelector(getVisibleSideBar);
   const { data: session, status } = useSession();
   return (
-    <div className="w-screen h-screen min-w-[360px] bg-neutral-900">
+    <div className="w-screen h-screen min-w-[360px] bg-neutral-900 overflow-y-hidden">
       <header>
         <Header />
       </header>
-      {status !== "loading" && (!session || !session?.user) && (
-        <div className="flex w-screen h-screen">
-          <div className="m-auto">
-            <div className="flex w-[320px] h-fit p-6 rounded-lg bg-slate-600">
-              <div className="w-full">
-                <div className=" text-center mb-4">
-                  로그인이 필요한 서비스 입니다.
-                </div>
-                <div className=" text-center mb-4">
-                  아래 링크를 통해서 로그인 해주세요.
-                </div>
-                <div className="flex">
-                  <div
-                    className="text-xl mx-auto text-center cursor-pointer underline decoration-solid"
-                    onClick={() => {
-                      signIn();
-                    }}>
-                    로그인
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+      {status !== "loading" && (!session || !session?.user) && <SigninAlert />}
 
       <main className="grid lg:grid-cols-6 grid-cols-1 w-screen min-w-[360px] h-[calc(100vh-56px)] max-h-[calc(100vh-56px)] overflow-hidden max-mobile:overflow-scroll overflow-y-hidden">
         <aside
@@ -45,7 +22,7 @@ const Layout = ({ children }: { children: JSX.Element }) => {
           } w-[240px] h-full bg-neutral-900`}>
           <SideBar />
         </aside>
-        <section className="min-w-[360px] lg:col-span-5 col-span-1 h-full max-h-full px-2">
+        <section className="min-w-[360px] lg:col-span-5 col-span-1 h-full max-h-full px-2 overflow-y-hidden">
           {children}
         </section>
       </main>
