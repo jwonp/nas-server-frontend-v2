418ce641800df751426c71d8e40151e260e8703c
Joowon <tkdel222@gmail.com>
2024-03-08 00:55:41 +0900

    update to disable favorite

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -28,20 +28,6 @@ const SideBar = () => {
         isVisibleSidebar ? "fixed" : "hidden"
       } w-[240px] h-full bg-neutral-900`}>
       <div className="w-full h-full left-0 top-56 py-4   border-zinc-100">
-        <div className="indent-2  text-white text-xl font-semibold font-['Inter']">
-        즐겨찾기
-        </div>
-        <div className="max-h-60 overflow-scroll">
-        <SideListBar />
-        <SideListBar />
-        <SideListBar />
-        <SideListBar />
-        <SideListBar />
-        <SideListBar />
-        <SideListBar />
-        <SideListBar />
-        <SideListBar />
-      </div>
         <RemainingStorageSize
           isLoading={volumeQuery.isLoading}
           volume={volumeQuery.data}
