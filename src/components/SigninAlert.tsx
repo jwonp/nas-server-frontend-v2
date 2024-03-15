cf9659adab3355d11205b05ee7ddd10ca5175fa8
Joowon <tkdel222@gmail.com>
2024-03-15 17:38:32 +0900

    update text color

--- src/components/SigninAlert.tsx
+++ src/components/SigninAlert.tsx
@@ -5,15 +5,15 @@ const SigninAlert = () => {
       <div className="m-auto">
         <div className="flex w-[320px] h-fit p-6 rounded-lg bg-slate-600">
           <div className="w-full">
-            <div className=" text-center mb-4">
+            <div className="text-white text-center mb-4">
               로그인이 필요한 서비스 입니다.
             </div>
-            <div className=" text-center mb-4">
+            <div className="text-white text-center mb-4">
               아래 링크를 통해서 로그인 해주세요.
             </div>
             <div className="flex">
               <div
-                className="text-xl mx-auto text-center cursor-pointer underline decoration-solid"
+                className="text-white text-xl mx-auto text-center cursor-pointer underline decoration-solid"
                 onClick={() => {
                   signIn();
                 }}>
