5279010d388e2f50956e0143099a5346ba69609d
Joowon <tkdel222@gmail.com>
2024-03-17 21:54:41 +0900

    update vitest migrated by jest

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    "next/babel",
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+  env: {
+    test: {
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        '@babel/plugin-transform-runtime'
+      ]
+    }
+  }
+
+};
