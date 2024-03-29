384892e3730029230976102e540ceedec3c2acaf
Joowon <tkdel222@gmail.com>
2024-03-29 22:08:17 +0900

    update close button location

--- src/components/Storage/Modal/common/MediaModal.tsx
+++ src/components/Storage/Modal/common/MediaModal.tsx
@@ -22,7 +22,7 @@ const MediaModal = ({ isVisibleMedia, children }: MediaModalProps) => {
       <div className="fixed flex z-0 top-0 left-0 w-screen h-screen bg-slate-950 opacity-90 " />
       <div className="flex my-auto w-full h-full px-10  z-40 relative">
         <figure
-          className="flex cursor-pointer absolute right-0"
+          className="flex cursor-pointer absolute right-1 top-2"
           onClick={() => {
             dispatch(resetMediaModal());
           }}>
